69e42219f537cf61de399d58e79a080de889970e
Vanessa Bennett <vbennett2@wisc.edu>
2024-12-28 11:19:14 -0600

    Remove folder from Git tracking

--- AlarmManagerBlog/app/src/main/java/com/nipunapps/alarmmanagerblog/AlarmItem.kt
+++
@@ -1,9 +0,0 @@
-package com.nipunapps.alarmmanagerblog
-
-import java.time.LocalDateTime
-
-//Contians the items for the Alarm. I could add other items too like plant?
-data class AlarmItem(
-    val alarmTime : LocalDateTime,
-    val message : String
-)
--- AlarmManagerBlog/app/src/main/java/com/nipunapps/alarmmanagerblog/AlarmScheduler.kt
+++
@@ -1,7 +0,0 @@
-package com.nipunapps.alarmmanagerblog
-
-//Methods that can be performed on the Alarm. Maybe I could add edit?
-interface AlarmScheduler {
-    fun schedule(alarmItem: AlarmItem)
-    fun cancel(alarmItem: AlarmItem)
-}
